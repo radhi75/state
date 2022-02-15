6ad94c6b9b3d9b900e54e477cacab2f2c4e7f5bf
radhi75 <https://github.com/radhi75/git.git>
2022-02-15 01:56:24 +0100

    msg

--- src/App.js
+++ src/App.js
@@ -1,13 +1,76 @@
 import "./App.css";
 
-import Textedimport from "./Components/Textedit/Textedit";
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <Textedimport />
-    </div>
-  );
+export class App extends Component {
+  constructor() {
+    super();
+    this.showsPerson = this.showsPerson.bind(this);
+    this.state = {
+      Person: {
+        fullName: "Radhi Aissa ",
+        bio: `hi it's me again .`,
+        imgSrc: "./me.jpg",
+        profession: "none",
+        interval: 0,
+        shows: false,
+      },
+    };
+  }
+
+  componentDidMount() {
+    setInterval(() => {
+      this.setState({
+        Person: {
+          ...this.state.Person,
+          interval: this.state.Person.interval + 1,
+        },
+      });
+    }, 2000);
+  }
+
+  showsPerson() {
+    this.setState({
+      Person: { ...this.state.Person, shows: !this.state.Person.shows },
+    });
+  }
+
+  getPersonProfile() {
+    const isShown = this.state.Person.shows;
+    if (isShown === true) {
+      return (
+        <div className=" wrapper">
+          <div className="container">
+            <h2 className="title"> {this.state.Person.fullName} </h2>
+            <img className="img" src={this.state.Person.imgSrc} alt="" />
+            <p className="info"> {this.state.Person.bio} </p>
+            <h2 className="job"> {this.state.Person.profession} </h2>
+          </div>
+        </div>
+      );
+    }
+    return "";
+  }
+
+  getToggler() {
+    const isShown = this.state.Person.shows;
+    if (isShown === true) {
+      return " shown";
+    }
+    return " not Shown";
+  }
+
+  render() {
+    return (
+      <div className="wrapper2">
+        {this.getPersonProfile()}
+        <button className="button" type="button" onClick={this.showsPerson}>
+          {this.getToggler()}
+        </button>
+        <p className="timer">{this.state.Person.interval}</p>
+      </div>
+    );
+  }
 }
 
 export default App;
